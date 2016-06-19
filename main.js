e70984e97380dfcedd12916d004d8c8a01be4040
Sergey Kolchinskiy <sergey.kolchinskiy@gmail.com>
2016-06-19 11:58:47 -0400

    merge

--- main.js
+++ main.js
@@ -5,6 +5,15 @@ $(document).ready(function(){
     $(this).toggleClass('button button-active')
     $('.show').toggleClass('show no-show')
     $('#'+$(this).attr('data-name')).toggleClass('show no-show')
+  });
+
+  var canvas = document.querySelector('canvas');
+  var ctx = canvas.getContext('2d');
+  canvas.addEventListener('mousemove',function(event){
+    var x = event.layerX;
+    var y = event.layerY;
+    var pixel = ctx.getImageData(x,y,1,1);
+    pixel.data = [255,255,255,0];
   })
 
 })
