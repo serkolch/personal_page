3576a4d0edd8ac261da2f8634efe6b6adfb0ad04
Sergey Kolchinskiy <sergey.kolchinskiy@gmail.com>
2016-08-10 12:17:41 -0400

    optimize

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
 
   $('.select section').on('click',function(){
-    $('.button-active').toggleClass('button button-active')
-    $(this).toggleClass('button button-active')
-    $('.show').toggleClass('show no-show')
-    $('#'+$(this).attr('data-name')).toggleClass('show no-show')
-  })
+    $('.button-active').toggleClass('button button-active');
+    $(this).toggleClass('button button-active');
+    $('.show').toggleClass('show no-show');
+    $('#'+$(this).attr('data-name')).toggleClass('show no-show');
+  });
 
-})
+});
